382d688d33113e2d11780590217136c859f94bed
Lloyd <lloydmeta@gmail.com>
2016-03-17 03:54:49 +0900

    Site updated at 2016-03-16 18:54:48 UTC

--- downloads/code/scala/WebcamSource.scala
+++ downloads/code/scala/WebcamSource.scala
@@ -1,3 +1,17 @@
+package com.beachape.video
+
+import akka.actor.{ DeadLetterSuppression, Props, ActorSystem, ActorLogging }
+import akka.stream.actor.ActorPublisher
+import akka.stream.actor.ActorPublisherMessage.{ Cancel, Request }
+import akka.stream.scaladsl.Source
+import org.bytedeco.javacpp.opencv_core._
+import org.bytedeco.javacv.{ FrameGrabber, Frame }
+import org.bytedeco.javacv.FrameGrabber.ImageMode
+
+/**
+ * Created by Lloyd on 2/13/16.
+ */
+
 object Webcam {
 
   /**
@@ -38,8 +52,8 @@ object Webcam {
     imageHeight: Int,
     bitsPerPixel: Int,
     imageMode: ImageMode
-  ) = synchronized {
-    val g = new OpenCVFrameGrabber(deviceId)
+  ): FrameGrabber = synchronized {
+    val g = FrameGrabber.createDefault(deviceId)
     g.setImageWidth(imageWidth)
     g.setImageHeight(imageHeight)
     g.setBitsPerPixel(bitsPerPixel)
